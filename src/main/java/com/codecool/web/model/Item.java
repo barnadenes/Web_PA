026e937ab596de16b3837744e1ede068496a0ed8
barnadenes <nyukk1007@gmail.com>
2019-08-07 16:22:03 +0200

    ITEM model DONE

--- src/main/java/com/codecool/web/model/Item.java
+++ src/main/java/com/codecool/web/model/Item.java
@@ -1,20 +1,38 @@
 package com.codecool.web.model;
 
 public final class Item extends AbstractModel{
-    private final String url;
     private final String title;
+    private final String author;
+    private final String url;
+    private final String plot;
+    private final int price;
 
-    public Item(int id, String url, String title) {
-        super(id);
-        this.url = url;
-        this.title = title;
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
     }
 
     public String getUrl() {
         return url;
     }
 
-    public String getTitle() {
-        return title;
+    public String getPlot() {
+        return plot;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public Item(int id, String title, String author, String url, String plot, int price) {
+        super(id);
+        this.title = title;
+        this.author = author;
+        this.url = url;
+        this.plot = plot;
+        this.price = price;
     }
 }
